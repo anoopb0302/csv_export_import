b540a320dee8882a2085ae3509bb79e21c1d4c1a
chetan <chetan.s@birdvision.in>
2015-10-30 18:28:54 +0530

    Remove changes releted to mailer

--- app/controllers/csv_codes_controller.rb
+++ app/controllers/csv_codes_controller.rb
@@ -1,12 +1,13 @@
 class CsvCodesController < ApplicationController
-layout 'csv_code'
+
+  layout 'csv_code'
+  
   def import
     CsvCode.import(params[:file])
     flash.now[:notice] = "codes added successfully"
     redirect_to csv_codes_path
-    
-
   end
+
   def index
     @total_codes_count = CsvCode.all.count
     @remaining_codes_count = CsvCode.where(used_at: nil).count
@@ -19,9 +20,8 @@ layout 'csv_code'
   
   def mobile_api
     check_code = CsvCode.where(used_at: nil).first
-    if check_code == nil
-      UserMailer.delay.codes_email
-      flash[:error] = "all codes are used already"
+    if check_code.nil?
+      return false
     else
       check_code.update_attributes(:mobile_no => params[:mobile_no], :used_at => Time.now)
       flash[:success] = "code is sent successfully" 
