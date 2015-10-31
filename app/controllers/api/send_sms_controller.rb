7347affbd81cf10ae65e62bb80340a10f8153ada
chetan <chetan.s@birdvision.in>
2015-10-31 14:23:28 +0530

    Added logger into api

--- app/controllers/api/send_sms_controller.rb
+++ app/controllers/api/send_sms_controller.rb
@@ -1,6 +1,9 @@
 class Api::SendSmsController < ApplicationController
 
 	def mobile_api
+		puts "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		puts params.inspect
+		puts "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
       check_code = CsvCode.where(used_at: nil).first
     	if check_code.nil?
       	  return false
