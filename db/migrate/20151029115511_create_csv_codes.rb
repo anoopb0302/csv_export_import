93c1e3f497707c83095d1db99c671d2a4b28210c
chetan <chetan.s@birdvision.in>
2015-11-04 12:58:08 +0530

    Added down migration

--- db/migrate/20151029115511_create_csv_codes.rb
+++ db/migrate/20151029115511_create_csv_codes.rb
@@ -1,7 +1,7 @@
 class CreateCsvCodes < ActiveRecord::Migration
   def change
     create_table :csv_codes do |t|
-      t.string :unique_codes
+      t.bigint :unique_codes, :limit => 10
       t.datetime :used_at , :default => nil
       t.string :mobile_no
       t.timestamps null: false
